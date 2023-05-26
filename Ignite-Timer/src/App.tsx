d6caa3c7c0e72d81ff412795ea3a6234658c416e
Willian <willianedi@hotmail.com>
2023-05-26 17:59:07 -0300

    upload

--- Ignite-Timer/src/App.tsx
+++ Ignite-Timer/src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
-import { Router } from "./components/Router";
+import { Router } from "./Router";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/themes/global";
